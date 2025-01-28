ff83199e0b455ad89452a1e6b8b32b28d1a5b459
Kosiarz <kosio>
2025-01-28 21:47:02 +0100

    fix nativewind styles

--- app/index.tsx
+++ app/index.tsx
@@ -6,6 +6,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '@/components/CustomButton';
 import { useGlobalContext } from '@/context/GlobalProvider';
+import "../global.css"
 
 export default function App() {
   // const {isLoading, isLoggedIn} = useGlobalContext();
